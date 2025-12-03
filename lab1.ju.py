1b9d2289399f15e22e2998c3d9b2ca20df6ecbd8
zggff <gigamaximwachau@gmail.com>
2025-12-03 23:04:54 +0300

    < 50 MSE

--- lab1.ju.py
+++ lab1.ju.py
@@ -12,6 +12,9 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolu
 from sklearn.feature_selection import RFE
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
 import os
+import seaborn as sns
+from scipy import stats
+from scipy.stats import norm, skew
 
 
 # %% [md]
@@ -21,7 +24,7 @@ df: pd.DataFrame = pd.read_csv("./data/train.csv")
 df = df.dropna()
 df = cast(pd.DataFrame, df[df['RiskScore'].abs() < 200])
 df["ApplicationDate"] = pd.to_datetime(df['ApplicationDate'])
-df.sort_values(by="RiskScore") # pyright: ignore[reportUnusedExpression]
+df.sort_values(by="RiskScore")  # pyright: ignore[reportUnusedExpression]
 
 
 # %%
@@ -34,78 +37,41 @@ for i, column in enumerate(df.columns):
     plt.title(column)
 
 
-# %% [md]
-# По графикам видно, что можно исключить JobTenure, EmploymentStatus,
-# EducationLevel, Experience, LengthOfCreditHistory,
-# PreviousLoanDefaults, PaymentHistory, BankruptcyHistory,
-# LoanPurpose, HomeOwnershipStatus, MaritalStatus, LoadDuration,
-# ApplicationDate
-
 
 # %%
-
-categoric = ["EducationLevel", "EmploymentStatus", 
-             "HomeOwnershipStatus",
-             "MaritalStatus", "LoanPurpose"]
-
-df2 = df.copy()
-le = LabelEncoder()
-for col in categoric:
-    df2[col] = le.fit_transform(df2[col])
-correlation_matrix = df2.corr()
-print(correlation_matrix.sort_values(by="RiskScore")["RiskScore"])
-
-
-
-# %%
-def KTest(X, y) -> Tuple[LinearRegression, List[float]]:
-    scores = []
-    model_best = LinearRegression(fit_intercept=False)
-    max_score = 1000
-
-    cv = KFold(n_splits=10, shuffle=True)
-    for train_index, test_index in cv.split(X_ohe):
-        model = LinearRegression(fit_intercept=False)
-        X_train, X_test, y_train, y_test = X[train_index], X[test_index], y.iloc[train_index], y.iloc[test_index]
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        scores.append(mean_squared_error(y_test, y_pred))
-        if scores[-1] < max_score:
-            model_best = model
-            max_score = scores[-1]
-    return model_best, scores
+y = df['RiskScore']
+X = df.drop(columns=['RiskScore'])
+categorical = X.select_dtypes(include=['object']).columns
+numerical = X.select_dtypes(include=[np.number]).columns
+le_dict = {}
 
 
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
-    selected = ["CreditScore", "MonthlyIncome", "BaseInterestRate", "TotalDebtToIncomeRatio"]
-    res = cast(pd.DataFrame, df[selected + categoric])
-    # repeats = ["AnnualIncome", "BaseInterestRate"]
-    # res = df.drop(columns=repeats)
+    res = df.drop(columns=["ApplicationDate"])
+    for col in categorical:
+        le = LabelEncoder()
+        res[col] = le.fit_transform(res[col])
+        le_dict[col] = le
     return res
 
 
-y = df['RiskScore']
-X = df.drop(columns=['RiskScore'])
 X = preprocess(X)
-enc = OneHotEncoder(sparse_output=True, handle_unknown="ignore")
-X_ohe = enc.fit_transform(X)
 
 
-model, scores = KTest(X_ohe, y)
-scores
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-# model.fit(X_train, y_train)
-# print("fitted model")
+model = LinearRegression()
+model.fit(X_train, y_train)
 
 
 # %%
-# X_train, X_test, y_train, y_test = train_test_split(X_ohe, y, test_size=0.1, random_state=1)
-#
-y_pred = model.predict(X_ohe)
-print(f"MSE  = {mean_squared_error(y, y_pred)}")
-print(f"MAE  = {mean_absolute_error(y, y_pred)}")
-print(f"MAPE = {mean_absolute_percentage_error(y, y_pred)}")
-print(f"R^2  = {r2_score(y, y_pred)}")
+y_pred = model.predict(X_test)
+print(f"MSE  = {mean_squared_error(y_test, y_pred)}")
+print(f"MAE  = {mean_absolute_error(y_test, y_pred)}")
+print(f"MAPE = {mean_absolute_percentage_error(y_test, y_pred)}")
+print(f"R^2  = {r2_score(y_test, y_pred)}")
 
 # %%
 plt.subplot()
@@ -119,12 +85,11 @@ path = "./out.csv"
 X_valid = pd.read_csv("./data/test.csv")
 X_valid = X_valid.drop(columns=["ID"])
 X_valid = preprocess(X_valid)
-X_valid = enc.transform(X_valid)
 print("prepared")
 y_valid = model.predict(X_valid)
 res = pd.DataFrame()
 res["ID"] = np.arange(len(y_valid))
-res["RiskScore"] = y_valid 
+res["RiskScore"] = y_valid
 res.set_index("ID", inplace=True)
 
 os.remove("./out.csv")
